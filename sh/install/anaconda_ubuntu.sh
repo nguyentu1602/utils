b1e4167eb91efa18b3e54c8f021219de7283894f
Cuong (James) Nguyen <cuong.nguyentumanh@gmail.com>
2019-12-22 21:33:36 +0000

    update to remove non-relevant flag

--- sh/install/anaconda_ubuntu.sh
+++ sh/install/anaconda_ubuntu.sh
@@ -1,5 +1,5 @@
 # install anaconda
 cd /tmp &
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh &
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3 -to-bash-profile yes &
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3 &
 cd $HOME
